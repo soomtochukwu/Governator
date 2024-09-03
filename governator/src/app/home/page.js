c3095e05cc06cd1a986994804ca90cd68734cbf8
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-09-03 19:42:05 +0100

    aaaa

--- governator/src/app/home/page.js
+++ governator/src/app/home/page.js
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-import {
-  HomeHeader,
-  NotificationCard,
-  HomeFilter,
-  HomeTracker,
-  HomeVoteCard,
-} from "../dashboard/helper";
-// import Link from "next/link";
-
-const Home = () => {
-  return (
-    <div className=" space-y-6 px-3 py-3 bg-[url('/Group.svg')] bg-[#010014]">
-      <HomeHeader
-        avatarImage={`/animoji2.png`}
-        logo={`/logo.png`}
-        avatarName={`franklivani`}
-      />
-      <HomeFilter drop={true} />
-      <div className="flex justify-between  gap-3 overflow-x-hidden ">
-        <section className="w-2/3 border px-4 space-y-4 py-3 transition">
-          {/* // Replace the following code with the actual content */}
-          <div
-            // href="/dashboard/Id"
-            className=" hover:border-b-2 shadow-md hover:bg-slate-500 bg-[url('/Frame.svg')]  px-1"
-          >
-            <HomeTracker tracker={true} />
-            <HomeVoteCard trend={true} />
-          </div>
-          <div
-            // href="/dashboard/Id"
-            className=" hover:border-b-2 shadow-md hover:bg-slate-500 bg-[url('/Frame.svg')]  px-1"
-          >
-            <HomeTracker tracker={false} />
-            <HomeVoteCard trend={false} />
-          </div>
-          <div
-            // href="/dashboard/Id"
-            className=" hover:border-b-2 shadow-md hover:bg-slate-500 bg-[url('/Frame.svg')]  px-1"
-          >
-            <HomeTracker tracker={null} />
-            <HomeVoteCard trend={true} />
-          </div>
-          <div
-            // href="/dashboard/Id"
-            className=" hover:border-b-2 shadow-md hover:bg-slate-500 bg-[url('/Frame.svg')]  px-1"
-          >
-            <HomeTracker tracker={null} />
-            <HomeVoteCard trend={true} />
-          </div>
-        </section>
-        <section className="w-2/5 border border-green-400 rounded-t-md max-h-fit">
-          <h1 className="bg-green-400 mb-3 px-3 py-2 space-y-2">
-            Onchain Notification
-          </h1>
-          <NotificationCard avater={"/animoji3.png"} />
-          <NotificationCard avater={"/animoji3.png"} />
-          <NotificationCard avater={"/animoji3.png"} />
-        </section>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
-=======
 import { HomeFilter } from "./homeFilter";
 import { NotificationCard, HomeTracker, HomeVoteCard } from "./helper";
 
@@ -104,4 +38,3 @@ const Home = () => {
 };
 
 export default Home;
->>>>>>> 4e86d79 (uodating)
--- governator/src/app/page.js
+++ governator/src/app/page.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import React from "react";
-
-const Home = () => {
-  return (
-    <div className="flex flex-col p-3 *:m-2 items-center">
-      <h1 className="text-5xl text-center">Governator</h1>
-
-    </div>
-  );
-};
-
-export default Home;
-=======
 import Link from "next/link";
 import React from "react";
 
@@ -25,4 +10,3 @@ const Home = () => {
 };
 
 export default Home;
->>>>>>> 4e86d79 (uodating)
