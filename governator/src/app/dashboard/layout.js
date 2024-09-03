09c630bcf15a0f2d13de018feb4244658f142210
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-09-03 19:42:05 +0100

    aaaa

--- governator/src/app/dashboard/layout.js
+++ governator/src/app/dashboard/layout.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import MenuBar from "./menubar";
-import SideBar from "./sideBar";
-
-export const metadata = {
-  title: "User Dashboard",
-};
-
-export default function DashboardLayout({ children }) {
-  return (
-    <main className="relative w-full h-screen overflow-hidden flex p-4 bg-[#e9e9e9] text-blue-950 gap-2">
-      <SideBar />
-      <div className="relative w-full">
-        <MenuBar />
-        <div className="w-full h-full overflow-x-hidden">
-            {children}
-        </div>
-      </div>
-    </main>
-  );
-}
-=======
 "use client";
 
 import MenuBar from "./menubar";
@@ -36,4 +14,3 @@ export default function DashboardLayout({ children }) {
     </main>
   );
 }
->>>>>>> 4e86d79 (uodating)
--- governator/src/app/signup/page.js
+++ governator/src/app/signup/page.js
@@ -2,81 +2,76 @@
 // http://localhost:3000/login
 // This page is responsible for rendering the login page
 
-<<<<<<< HEAD
-import { Fragment, useState } from "react";
-=======
-import { useState } from "react";
->>>>>>> 4e86d79 (uodating)
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
+export default function Signup() {
+  // Changed the component name
 
-export default function Signup() { // Changed the component name
+  const router = useRouter();
 
-	const router = useRouter();
+  const handleConnectWallet = () => {
+    // Redirect to wallet connection logic or page
+    router.push("/connect-wallet");
+  };
 
-	const handleConnectWallet = () => {
-		// Redirect to wallet connection logic or page
-		router.push("/connect-wallet");
-	};
+  return (
+    <Fragment>
+      <div className="min-h-screen flex">
+        {/* Left Div */}
+        <div className="flex-1 flex flex-col justify-center items-center bg-white p-8">
+          <div className="w-full max-w-md animate-fade-in-down">
+            <div className="flex justify-center mb-6">
+              <Image
+                src="/welcomeback.svg"
+                alt="Governator Logo"
+                width={300}
+                height={300}
+                className="object-contain"
+              />
+            </div>
+            <h1 className="text-2xl font-bold text-center mb-2 text-[#0E0E2C] animate-fade-in">
+              Login {/* Adjusted the title */}
+            </h1>
+            <p className="text-center text-black mb-6 animate-pulse">
+              Welcome, Click Below to connect wallet
+            </p>
+            <form onSubmit={handleConnectWallet}>
+              <button
+                type="submit"
+                className="w-full py-3 mt-4 border border-gray-300 text-sm font-medium rounded-md text-white bg-[#0E0E2C] hover:bg-[#262670] hover:text-white transition-all duration-300"
+              >
+                CONNECT WALLET
+              </button>
+            </form>
+          </div>
+          <div className="mt-6 text-center text-sm animate-fade-in">
+            <span className="text-black">
+              If you don't have an account with us,
+              <Link
+                href="/signup"
+                className="text-green-600 hover:text-green-500 transition-all duration-300"
+              >
+                Register here
+              </Link>
+            </span>
+          </div>
+        </div>
 
-	return (
-		<Fragment>
-			<div className="min-h-screen flex">
-				{/* Left Div */}
-				<div className="flex-1 flex flex-col justify-center items-center bg-white p-8">
-					<div className="w-full max-w-md animate-fade-in-down">
-						<div className="flex justify-center mb-6">
-							<Image
-								src="/welcomeback.svg"
-								alt="Governator Logo"
-								width={300}
-								height={300}
-								className="object-contain"
-							/>
-						</div>
-						<h1 className="text-2xl font-bold text-center mb-2 text-[#0E0E2C] animate-fade-in">
-							Login {/* Adjusted the title */}
-						</h1>
-						<p className="text-center text-black mb-6 animate-pulse">
-							Welcome, Click Below to connect wallet
-						</p>
-						<form onSubmit={handleConnectWallet}>
-							<button
-								type="submit"
-								className="w-full py-3 mt-4 border border-gray-300 text-sm font-medium rounded-md text-white bg-[#0E0E2C] hover:bg-[#262670] hover:text-white transition-all duration-300"
-							>
-								CONNECT WALLET
-							</button>
-						</form>
-					</div>
-					<div className="mt-6 text-center text-sm animate-fade-in">
-						<span className="text-black">
-							If you don't have an account with us,
-							<Link
-								href="/signup"
-								className="text-green-600 hover:text-green-500 transition-all duration-300"
-							>
-								Register here
-							</Link>
-						</span>
-					</div>
-				</div>
-
-				{/* Right Div */}
-				<div className="hidden lg:flex flex-1 bg-gray-900 text-white items-center justify-center">
-					<div className="w-full max-w-lg">
-						<Image
-							src="/Hidden-mining-bro2.svg"
-							alt="Governator image"
-							width={1000}
-							height={1000}
-							className="object-contain animate-fade-in-left"
-						/>
-					</div>
-				</div>
-			</div>
-		</Fragment>
-	);
+        {/* Right Div */}
+        <div className="hidden lg:flex flex-1 bg-gray-900 text-white items-center justify-center">
+          <div className="w-full max-w-lg">
+            <Image
+              src="/Hidden-mining-bro2.svg"
+              alt="Governator image"
+              width={1000}
+              height={1000}
+              className="object-contain animate-fade-in-left"
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
