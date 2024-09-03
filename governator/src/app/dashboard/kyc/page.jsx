edf9034cbfa488cdded5d67afa7c69f8a3e67920
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-09-03 02:52:23 +0100

    layout update

---
+++ governator/src/app/dashboard/kyc/page.jsx
@@ -0,0 +1,22 @@
+
+
+
+const KycVerification = () => {
+  return (
+    <>
+      <h1>kyc</h1>
+      <section>
+        <header></header>
+        <nav>
+          <ul>
+            <li>
+                <a href="/dashboard/kyc/verification">Verification</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </>
+  );
+};
+
+export default KycVerification;
--- governator/src/app/layout.js
+++ governator/src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import HeaderLogo from "../../components/HeaderLogo";
 
-import { usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {pathname !== "/dashboard" ? <HeaderLogo /> : null}
+        {pathname === "/login" ? <HeaderLogo /> : null}
         {children}
       </body>
     </html>
