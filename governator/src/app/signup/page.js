014dcb15f0848b44ef9234e0fd5db4fb137ca783
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-09-03 19:51:40 +0100

    fragment

--- governator/src/app/signup/page.js
+++ governator/src/app/signup/page.js
@@ -1,10 +1,10 @@
 "use client";
 // http://localhost:3000/login
 // This page is responsible for rendering the login page
-
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment } from "react";
 
 export default function Signup() {
   // Changed the component name
