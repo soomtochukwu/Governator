d9efdd1f5b246717da0f56ed57a0592959a087e8
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-09-09 01:33:39 +0100

    landing page

--- governator/src/app/layout.tsx
+++ governator/src/app/layout.tsx
@@ -1,26 +1,17 @@
 "use client";
-import Header from "../components/HeaderLogo";
-//
-
 import "./globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-
 import { Providers } from "./providers";
-import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const path = usePathname();
   return (
     <html lang="en">
-      <body className={"w-screen flex text-center items-center justify-center"}>
-        <Providers>
-          {path == "/" ? <Header /> : null}
-          {children}
-        </Providers>
+      <body className={"bg-white"}>
+        <Providers>{children}</Providers>
+        
       </body>
     </html>
   );
--- governator/src/app/useContext.js
+++ governator/src/app/useContext.js
@@ -1,1 +1,39 @@
+import React, { createContext, useReducer } from "react";
+// import { ActionType } from "./common/actiontype";
 
+const PostContext = createContext("post");
+
+const INITIALSTATE = {
+  menuState: false, // menu state
+  theme: "light",
+  store: [], // store data
+  style: {}, // style state
+};
+
+const actionType = {
+  THEME: "Theme",
+  OPEN: "Open",
+  CLOSE: "Close",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionType.THEME:
+      return { ...state, theme: "dark" };
+    case actionType.style:
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+};
+
+const PostProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, INITIALSTATE);
+  return (
+    <PostContext.Provider value={[state, dispatch]}>
+      {children}
+    </PostContext.Provider>
+  );
+};
+
+export { PostContext, PostProvider, INITIALSTATE, reducer };
